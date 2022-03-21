61200f2872e86b052e057db738614918a28e2d00
Gingaling <nycgingsf@hotmail.com>
2022-03-20 23:40:04 -0700

    See additional comments in Header file.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Re-read React documentation sections on props, components, hooks, lifting state, and other sections. I wanted to connect the generate image compoinent to an onClick event. My code started getting crazy again.
+
+// Then researched react random generators for advice and image generators for cats and dogs, looking at different approaches.
+
+import React from 'react';
+import './App.css';
+
+function Header() {
+	return (
+		<div className="header___main">
+			<h1>Cats</h1>
+		</div>
+	);
+}
+
+export default Header;
